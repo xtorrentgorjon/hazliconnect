598131a5593cb6293d791ea3a7c1cf6dc18e76e6
Xavier Torrent Gorjon <x.torrent@gerimedica.nl>
2019-06-06 09:54:02 +0200

    Removed extra parameters from the app.py to the kubernetes configmap.

--- app/app.py
+++ app/app.py
@@ -1,28 +1,46 @@
 from flask import Flask, render_template, redirect, url_for, session, request, jsonify, make_response
 from flask_oauthlib.client import OAuth
+from pygelf import GelfUdpHandler
 import urllib.request
 
 import datetime
+import logging
+import os
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
 oauth = OAuth(app)
 
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 ENVIRONMENT = "test"
 RELEASE_TIME = datetime.datetime.now()
 
-PAGE_INFO = {"version":VERSION, "environment":ENVIRONMENT, "release_time":RELEASE_TIME}
+PAGE_INFO = {"version":VERSION,
+            "environment":ENVIRONMENT,
+            "release_time":str(RELEASE_TIME)}
+
+LOG_PARAM = {"ip":os.environ["LOG_ENDPOINT_IP"],
+            "port":int(os.environ["LOG_ENDPOINT_PORT"])}
+
+OAUTH_PARAM = {"url":os.environ["OAUTH_GITLAB_URL"],
+                "key":os.environ["OAUTH_GITLAB_KEY"],
+                "secret":os.environ["OAUTH_GITLAB_SECRET"]}
+
+logging.basicConfig(level=logging.INFO)
+gelflogger = logging.getLogger()
+gelflogger.addHandler(GelfUdpHandler(host=LOG_PARAM["ip"], port=LOG_PARAM["port"]))
+gelflogger.info('Starting Hazliconnect {}'.format(VERSION))
+
 
 gitlab = oauth.remote_app('gitlab',
-    base_url='https://gitlab.home.sendotux.net/api/v3/',
+    base_url='{}/api/v3/'.format(OAUTH_PARAM["url"]),
     request_token_url=None,
-    access_token_url='https://gitlab.home.sendotux.net/oauth/token',
-    authorize_url='https://gitlab.home.sendotux.net/oauth/authorize',
+    access_token_url='{}/oauth/token'.format(OAUTH_PARAM["url"]),
+    authorize_url='{}/oauth/authorize'.format(OAUTH_PARAM["url"]),
     access_token_method='POST',
-    consumer_key='9e55b76ff6dc10f29c1d57c9e1ce3faa54ad256149c110e47a16eb81cea9b1ce',
-    consumer_secret='8b8ea773b2c6e61ac8a539510ba6e75d4c024c97b922a363f1408c3808007288'
+    consumer_key='{}'.format(OAUTH_PARAM["key"]),
+    consumer_secret='{}'.format(OAUTH_PARAM["secret"])
 )
 
 @app.route('/')
@@ -30,6 +48,7 @@ def index():
     if 'gitlab_token' in session:
         me = gitlab.get('user')
         PAGE_INFO["rightside_link"] = "logout"
+        gelflogger.info('New client reaching the index page!')
         return render_template('index.html', data=me.data, page_info=PAGE_INFO)
         #return jsonify(me.data)
     PAGE_INFO["rightside_link"] = "index"
